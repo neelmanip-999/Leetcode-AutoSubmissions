3700438625faa613e17a14c543f21a24a1763d8a
Neelmani Pandey <neelmani.p999@gmail.com>
2025-07-22 08:33:38 +0530

    Time: 50 ms (72.00%) | Memory: 61.5 MB (12.83%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.java
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -1,22 +1,57 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
         int n = nums.length;
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        Set<Integer> seen = new HashSet<>();
         int st = 0;
         int end = 0;
-        int ans = 0;
         int sum = 0;
+        int ans = 0;
         while(end < n){
-            hm.put(nums[end], hm.getOrDefault(nums[end], 0)+1);
-            while(hm.get(nums[end]) > 1 && end >= st){
-                hm.put(nums[st], hm.get(nums[st])-1);
+            while(seen.contains(nums[end])){
+                seen.remove(nums[st]);
                 sum -= nums[st];
                 st++;
             }
+            seen.add(nums[end]);
             sum += nums[end];
             ans = Math.max(ans, sum);
-            end++;           
+            end++;
         }
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int maximumUniqueSubarray(int[] nums) {
+//         int n = nums.length;
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+//         int st = 0;
+//         int end = 0;
+//         int ans = 0;
+//         int sum = 0;
+//         while(end < n){
+//             hm.put(nums[end], hm.getOrDefault(nums[end], 0)+1);
+//             while(hm.get(nums[end]) > 1 && end >= st){
+//                 hm.put(nums[st], hm.get(nums[st])-1);
+//                 sum -= nums[st];
+//                 st++;
+//             }
+//             sum += nums[end];
+//             ans = Math.max(ans, sum);
+//             end++;           
+//         }
+//         return ans;
+//     }
+// }
