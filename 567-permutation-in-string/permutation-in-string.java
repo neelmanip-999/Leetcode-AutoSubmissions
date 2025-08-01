cf38b2257d8b74eafad6b41ed575e570faf65539
Neelmani Pandey <neelmani.p999@gmail.com>
2025-08-01 23:31:24 +0530

    Time: 5 ms (96.23%) | Memory: 42.7 MB (67.76%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,21 +1,53 @@
+// class Solution {
+//     public boolean checkInclusion(String s1, String s2) {
+//         if (s1.length() > s2.length()) return false;
+
+//         int[] freq1 = new int[26];
+//         int[] freq2 = new int[26];
+
+//         for (int i = 0; i < s1.length(); i++) {
+//             freq1[s1.charAt(i) - 'a']++;
+//             freq2[s2.charAt(i) - 'a']++;
+//         }
+
+//         if (Arrays.equals(freq1, freq2)) return true;
+
+//         for (int i = s1.length(); i < s2.length(); i++) {
+//             freq2[s2.charAt(i) - 'a']++;
+//             freq2[s2.charAt(i - s1.length()) - 'a']--;
+//             if (Arrays.equals(freq1, freq2)) return true;
+//         }
+
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if (s1.length() > s2.length()) return false;
 
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
+        int[] need = new int[26];
+        int[] window = new int[26];
 
-        for (int i = 0; i < s1.length(); i++) {
-            freq1[s1.charAt(i) - 'a']++;
-            freq2[s2.charAt(i) - 'a']++;
+        for (char c : s1.toCharArray()) {
+            need[c - 'a']++;
         }
 
-        if (Arrays.equals(freq1, freq2)) return true;
+        int left = 0, right = 0;
+        int valid = 0;
 
-        for (int i = s1.length(); i < s2.length(); i++) {
-            freq2[s2.charAt(i) - 'a']++;
-            freq2[s2.charAt(i - s1.length()) - 'a']--;
-            if (Arrays.equals(freq1, freq2)) return true;
+        while (right < s2.length()) {
+            char c = s2.charAt(right);
+            window[c - 'a']++;
+            right++;
+
+            while (right - left == s1.length()) {
+                if (Arrays.equals(need, window)) return true;
+
+                char d = s2.charAt(left);
+                window[d - 'a']--;
+                left++;
+            }
         }
 
         return false;
