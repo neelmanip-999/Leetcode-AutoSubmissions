fffee4dc2e172d2b2cafe4e1293aca187f7e7e82
Neelmani Pandey <neelmani.p999@gmail.com>
2025-08-28 14:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (39.49%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.java
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -21,9 +21,9 @@ class Solution {
         boolean left = checkTree(root.left);
         boolean right = checkTree(root.right);
         boolean check = false;
-        int i = root.left != null ? root.left.val:0;
-        int j = root.right != null ? root.right.val:0;
-        if(root.val == i + j){
+        // int i = root.left != null ? root.left.val:0;
+        // int j = root.right != null ? root.right.val:0;
+        if(root.val == root.left.val + root.right.val){
             check = true;
         }
         return left && right && check;
