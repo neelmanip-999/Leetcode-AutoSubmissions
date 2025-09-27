3711f3a73667a2e48752b6d3453b42716fa3f4db
Neelmani Pandey <neelmani.p999@gmail.com>
2025-09-27 19:30:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.02%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int lo = 0;
+        int hi = num;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if(Math.pow(mid, 2) == num){
+                return true;
+            }
+            else if(Math.pow(mid, 2) >= num){
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        return false;
+    }
+}
