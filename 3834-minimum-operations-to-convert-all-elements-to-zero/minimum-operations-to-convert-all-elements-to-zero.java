ac7f7bce236b78fa05f8d4e7408823760f52fb8c
Neelmani Pandey <neelmani.p999@gmail.com>
2025-11-11 00:47:08 +0530

    Time: 157 ms (28.17%) | Memory: 123.3 MB (19.72%) - LeetSync

---
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() > num) stack.pop();
+            if (stack.isEmpty() || stack.peek() < num) {
+                ans++;
+                stack.push(num);
+            }
+        }
+        return ans;
+    }
+}
