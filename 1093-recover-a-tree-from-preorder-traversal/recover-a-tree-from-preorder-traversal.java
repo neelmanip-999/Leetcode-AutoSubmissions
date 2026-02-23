62fcc4746ba70ec9bbdbb42ecf6ad710d7004a3d
Neelmani Pandey <neelmani.p999@gmail.com>
2026-02-24 03:07:23 +0530

    Time: 7 ms (35.62%) | Memory: 46.7 MB (43.15%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
@@ -14,42 +14,47 @@
  * }
  */
 class Solution {
-    private int n;
-    
-    private TreeNode solve(String traversal, int[] index, int depth) {
-        if (index[0] >= n) {
-            return null;
-        }
-        
-        int j = index[0];
-        while (j < n && traversal.charAt(j) == '-') {
-            j++;
-        }
-        
-        int dash = j - index[0];
-        
-        if (depth != dash) {
-            return null;
-        }
-        
-        index[0] += dash;
-        
-        int value = 0;
-        while (index[0] < n && Character.isDigit(traversal.charAt(index[0]))) {
-            value = value * 10 + (traversal.charAt(index[0]) - '0');
-            index[0]++;
-        }
-        
-        TreeNode root = new TreeNode(value);
-        root.left = solve(traversal, index, depth + 1);
-        root.right = solve(traversal, index, depth + 1);
-        
-        return root;
-    }
-    
     public TreeNode recoverFromPreorder(String traversal) {
-        n = traversal.length();
-        int[] index = {0}; // Using an array to pass by reference
-        return solve(traversal, index, 0);
+        Stack<TreeNode> stack = new Stack<>();
+        int i = 0;
+        while(i < traversal.length()){
+            int depth = 0;
+            while(i < traversal.length() && traversal.charAt(i) == '-'){
+                depth++;
+                i++;
+            }
+            int value = 0;
+            while(i < traversal.length() && Character.isDigit(traversal.charAt(i))){
+                value = value*10 + (traversal.charAt(i) - '0');
+                i++;
+            }
+
+            TreeNode node = new TreeNode(value);
+            while(stack.size() > depth){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                if(stack.peek().left == null){
+                    stack.peek().left = node;
+                }
+                else{
+                    stack.peek().right = node;
+                }
+            }
+            stack.push(node);
+        }
+        return stack.firstElement();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
