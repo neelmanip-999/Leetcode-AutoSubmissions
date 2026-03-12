65a6780c5e95612bc1bac2845feedc817b228730
Neelmani Pandey <neelmani.p999@gmail.com>
2026-03-12 22:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (35.13%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,12 +1,11 @@
 class Solution {
     public int findComplement(int num) {
-        String binStr = Integer.toBinaryString(num);
-        StringBuilder sb = new StringBuilder();
-        for(char ch : binStr.toCharArray()){
-            if(ch == '0') sb.append('1');
-            else sb.append('0');
+        int mask = 0;
+        int temp = num;
+        while (temp > 0) {
+            mask = (mask << 1) | 1; 
+            temp >>= 1; 
         }
-        int number = Integer.parseInt(sb.toString(), 2);
-        return number;
+        return num ^ mask;
     }
 }
