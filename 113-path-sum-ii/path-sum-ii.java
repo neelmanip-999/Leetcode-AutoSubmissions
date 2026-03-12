5e281d558172a2061d2930762ec12852cf48eabf
Neelmani Pandey <neelmani.p999@gmail.com>
2026-03-13 03:19:14 +0530

    Time: 1 ms (99.95%) | Memory: 45.6 MB (53.07%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -1,38 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    List<List<Integer>> ans;    
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> result = new ArrayList<>();
-        dfs(root, targetSum, new ArrayList<>(), result);
-        return result;
+        ans = new ArrayList<>();
+        Solve(root, targetSum, new ArrayList<>());
+        return ans;
     }
 
-    private void dfs(TreeNode node, int remainingSum, List<Integer> path, List<List<Integer>> result) {
-        if (node == null) return;
-
-        path.add(node.val);
-
-        if (node.left == null && node.right == null && remainingSum == node.val) {
-            result.add(new ArrayList<>(path));
-        } else {
-            dfs(node.left, remainingSum - node.val, path, result);
-            dfs(node.right, remainingSum - node.val, path, result);
+    public void Solve(TreeNode root, int targetSum, List<Integer> ll){
+        if (root == null) {
+            return;
         }
-
-        // Backtrack
-        path.remove(path.size() - 1);
+        ll.add(root.val);        
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            ans.add(new ArrayList<>(ll));
+        } else {
+            Solve(root.left, targetSum - root.val, ll);
+            Solve(root.right, targetSum - root.val, ll);
+        }        
+        ll.remove(ll.size() - 1);
     }
 }
